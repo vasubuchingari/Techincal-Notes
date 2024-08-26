ba633eb5bed0651c74f6d480a4fe411b128e8fb5
Vasu Buchingari <vasubuchingari@gmail.com>
2024-08-26 12:57:51 +0530

    Create inbuilt_functions_output.php

---
+++ PHP practises/Inbuilt Functions (Arrays,Strings) & oops Concept/inbuilt_functions_output.php
@@ -0,0 +1,298 @@
+PHP-STRING INBUILT FUNCTIONS & CONCEPTS
+STRLEN
+12
+STR-WORD-COUNT
+2
+STRPOS
+6
+STRTOLOWER
+hello world!
+
+
+STR-REPLACE
+Hello World! i am python language php is dynamically typed
+TRIM
+Hello    World!
+SUM
+sum of (5,10) =15
+sum of (10,16) =26
+sum of (20,30) =50
+
+ADD
+initial num value is 10 
+update value of num is 20 
+
+SUM OF TOTAL
+100
+FIRST NAME & LAST NAME
+HI, vs bu . 
+HI, as bu . 
+HI, dv bu . 
+ADD NUMBER
+6.4
+PHP-ARRAY INBUILT FUNCTIONS & CONCEPTS
+print list
+Array
+(
+    [0] => volvo
+    [1] => BMW
+    [2] => toyoto
+)
+
+ARRAY-PUSH
+volvo 
+BMW 
+Toyoto 
+Array
+(
+    [0] => volvo
+    [1] => BMW
+    [2] => Toyoto
+    [3] => Ford
+)
+
+print array
+Array
+(
+    [brand] => ford
+    [model] => Mustang
+    [year] => 1964
+)
+print  model,year in the array
+Mustang
+2002
+print keys & values
+brand: ford 
+model: Mustang 
+year: 2002 
+
+print keys & values
+monin : moye moye 
+govi : history repeats 
+gun : vundu status pettestha 
+hem : maruthi sir 
+vomm : pilla na koduku 
+Append into the list
+Array
+(
+    [0] => tata1
+    [1] => tata2
+    [2] => tata2
+    [3] => tata2
+)
+
+Add into the list
+Array
+(
+    [0] => apple
+    [1] => banana
+    [2] => cherry
+    [fruits] => guava
+)
+
+List fetch
+Toyoto
+Get year from the list
+
+2003
+UNSET referenced(&)
+Array
+(
+    [0] => ford
+    [1] => ford
+    [2] => ford
+)
+
+UNSET bmw
+Array
+(
+    [0] => volvo
+    [2] => toyoto
+)
+
+ARRAY-SPLICE
+Array
+(
+    [0] => Volvo
+    [1] => Toyota
+)
+
+
+UNSET (model)
+Array
+(
+    [brand] => Ford
+    [year] => 1964
+)
+
+ARRAY-COLUMNS
+Array
+(
+    [0] => john
+    [1] => jane
+    [2] => jos
+)
+
+
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+)
+
+ARRAY-COMBINE
+Array
+(
+    [a] => 1
+    [b] => 2
+    [c] => 3
+)
+
+ARRAY-COUNT-VALUES
+Array
+(
+    [yello] => 1
+    [red] => 1
+    [pink] => 2
+    [green] => 3
+)
+
+ARRAY-DIFF
+Array
+(
+    [0] => 1
+    [1] => 2
+)
+
+ARRAY-KEY-EXISTS
+name exists
+ARRAY-KEYS
+Array
+(
+    [0] => john
+    [1] => jas
+    [2] => zooa
+)
+
+ARRAY-MERGE
+Array
+(
+    [0] => apple
+    [1] => banana
+    [2] => orange
+    [3] => grape
+    [4] => kiwi
+    [5] => orange
+)
+
+ARRAY-SUM
+22
+ARRAY-UNIQUE
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+    [3] => 4
+    [7] => 6
+    [8] => 7
+)
+
+ARRAY-COMPACT
+Array
+(
+    [name] => vasy
+    [age] => 12
+    [city] => nellore
+)
+
+ARRAY-COUNT
+5
+ARRAY-(SHUFFLE)
+Array
+(
+    [0] => Ace
+    [1] => 9
+    [2] => Jack
+    [3] => 10
+    [4] => Queen
+    [5] => King
+    [6] => 8
+)
+
+SORT
+Array
+(
+    [0] => 2
+    [1] => 3
+    [2] => 4
+    [3] => 6
+    [4] => 8
+)
+
+RSORT
+Array
+(
+    [0] => 8
+    [1] => 6
+    [2] => 4
+    [3] => 3
+    [4] => 2
+)
+
+ASORT
+Array
+(
+    [Bob] => 20
+    [John] => 25
+    [Alice] => 30
+)
+
+ARSORT
+Array
+(
+    [Alice] => 30
+    [John] => 25
+    [Bob] => 20
+)
+
+KSORT
+Array
+(
+    [Alice] => 30
+    [Bob] => 20
+    [John] => 25
+)
+
+KRSORT
+Array
+(
+    [John] => 25
+    [Bob] => 20
+    [Alice] => 30
+)
+
+USORT
+Array
+(
+    [0] => Bob
+    [1] => John
+    [2] => Alice
+)
+
+UASORT
+Array
+(
+    [Bob] => 20
+    [John] => 25
+    [Alice] => 30
+)
+
+
+
+
+PHP-OOPS-CONCEPT
+Public propertyPublic method
+Protected property
+Private property
